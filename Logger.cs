2aa46ac140fe11ed06f448d51c9b3655f035995a
Valerian5788 <valerianvdcpro@gmail.com>
2024-10-05 14:25:31 +0200

    better logger, with rotation

--- Logger.cs
+++ Logger.cs
@@ -1,18 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EmailViewer
 {
     public static class Logger
     {
+        private const int MaxLogFiles = 7;
+        private const long MaxFileSizeBytes = 10 * 1024 * 1024; // 10 MB
+
         public static void Log(string message)
         {
-            string logPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "app.log");
-            File.AppendAllText(logPath, $"{DateTime.Now}: {message}{Environment.NewLine}");
+            string logDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs");
+            Directory.CreateDirectory(logDirectory);
+
+            string currentLogPath = Path.Combine(logDirectory, $"app_{DateTime.Now:yyyyMMdd}.log");
+
+            if (File.Exists(currentLogPath) && new FileInfo(currentLogPath).Length > MaxFileSizeBytes)
+            {
+                RotateLogs(logDirectory);
+            }
+
+            File.AppendAllText(currentLogPath, $"{DateTime.Now}: {message}{Environment.NewLine}");
+        }
+
+        private static void RotateLogs(string logDirectory)
+        {
+            var logFiles = Directory.GetFiles(logDirectory, "app_*.log")
+                                    .OrderByDescending(f => f)
+                                    .ToList();
+
+            for (int i = logFiles.Count - 1; i >= 0; i--)
+            {
+                if (i >= MaxLogFiles - 1)
+                {
+                    File.Delete(logFiles[i]);
+                }
+                else
+                {
+                    File.Move(logFiles[i], logFiles[i].Replace(".log", $"_{i + 1}.log"));
+                }
+            }
         }
     }
 }
